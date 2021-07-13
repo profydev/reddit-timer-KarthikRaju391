12a61bb3a4cfd6bc274c5ed282fe98153c20e2c8
KarthikRaju391 <karthikraju391@gmail.com>
2021-07-13 17:20:50 +0530

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import Header from './Components/header';
 import Home from './Components/home';
 import Search from './Components/search';
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/search">
-            <Search />
-          </Route>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
