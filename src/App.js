12fa4de19e6ca5a339ea582ebd8401877300f1f8
KarthikRaju391 <karthikraju391@gmail.com>
2021-07-13 14:41:31 +0530

    Created app skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      App Placeholder
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<Switch>
+					<Route exact path="/">
+						<div>Home</div>
+					</Route>
+					<Route exact path="/search">
+						<div>Search</div>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
