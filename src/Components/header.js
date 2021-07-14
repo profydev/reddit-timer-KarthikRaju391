568f204b9188f6ac58a1bc5a3692fe46bd60d83b
KarthikRaju391 <karthikraju391@gmail.com>
2021-07-14 15:03:26 +0530

    Added logo and links

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  max-width: 1300px;
+  width: 80%;
+  margin: 0 auto;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 1.6em;
+
+  img{
+    display: block;
+  }
+`;
+
+const NavLinks = styled.ul`
+  list-style-type: none;
+  font-family: 'Montserrat', sans-serif;
+  display: flex;
+  gap: 1.6em;
+`;
+
+const Links = styled.a`
+  text-decoration: none;
+  color: black;
+`;
 
 const Header = () => (
-  <header>
-    Header Placeholder
-  </header>
+  <Container>
+    <Nav>
+      <a href="/"><img src="/logo.svg" alt="logo" /></a>
+      <NavLinks>
+        <li><Links href="/searchpage">Search</Links></li>
+        <li><Links href="#how-it-works">How it works</Links></li>
+        <li><Links href="#about">About</Links></li>
+      </NavLinks>
+    </Nav>
+  </Container>
 );
 
 export default Header;
