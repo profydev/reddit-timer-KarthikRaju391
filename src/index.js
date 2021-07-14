c2eb940b443e5bcf49704e22b33a94fd7067cd33
Karthik Raju <karthikraju391@gmail.com>
2021-07-14 13:41:48 +0530

    Create app skeleton (#2)
    * Created the app skeleton
    
    * Created app skeleton
    
    * Change app title and meta description

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
 import App from './App';
 
-// eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line import/prefer-default-export
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const Root = () => (
+  <>
+    <GlobalStyle />
+    <App />
+  </>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
