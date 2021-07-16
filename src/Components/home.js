509adf8f0572f2809ede4dff9341c5f59c705b84
KarthikRaju391 <karthikraju391@gmail.com>
2021-07-16 19:02:53 +0530

    Add info section

--- src/Components/home.js
+++ src/Components/home.js
@@ -52,6 +52,33 @@ const Heatmap = styled.img`
   padding: 12px 82px;
 `;
 
+const InfoSection = styled.section`
+  max-width: 650px;
+  margin: 0 auto;
+  margin-top: 133px;
+`;
+
+const InfoHead = styled(Heading)`
+  font-size: 24px;
+  font-weight: 400;
+  text-align: justify;
+`;
+
+const InfoContent = styled.p`
+  color: #93918F;
+  line-height: 1.5em;
+
+`;
+
+const InfoSection2 = styled(InfoSection)`
+  margin-top: 105px;
+`;
+
+const SubLinks = styled.a`
+  text-decoration: none;
+  color: lightskyblue;
+`;
+
 const Home = () => (
   <div>
     <div>
@@ -67,6 +94,29 @@ const Home = () => (
     <ImageContainer to="/searchpage">
       <Heatmap src="/table.png" alt="heatmap" />
     </ImageContainer>
+    <InfoSection id="how-it-works">
+      <InfoHead>
+        How it works
+      </InfoHead>
+      <InfoContent>
+        • We find the 500 top posts from the past year for a subreddit.
+        <br />
+        • The data is visualized in a heatmap grouped by weekday and hour of the day.
+        <br />
+        • See immediately when to submit your reddit post.
+      </InfoContent>
+    </InfoSection>
+    <InfoSection2>
+      <InfoHead>About</InfoHead>
+      <InfoContent>
+        This app was created during a course on
+        <SubLinks rel="noreferrer" target="_blank" href="https://profy.dev/"> profy.dev </SubLinks>
+        with the goal to implement a
+        pixel-perfect real-world application with professional workflows and
+        tools like Kanban, Asana, Zeplin, GitHub, pull requests and code reviews.
+        <SubLinks rel="noreferrer" target="_blank" href="https://profy.dev/employers"> Click here for more information. </SubLinks>
+      </InfoContent>
+    </InfoSection2>
   </div>
 );
 
