69e459c75bf0174e95b65ba868719ec01209bff9
KarthikRaju391 <karthikraju391@gmail.com>
2021-07-18 13:40:26 +0530

    changed the url for the search link

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,47 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render,screen } from '@testing-library/react';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
-test('renders App', () => {
-  render(<App />);
+function setup() {
+  return render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+}
+
+describe('Header', () => {
+  test('Nav links points to the correct page', () => {
+    setup();
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    userEvent.click(aboutLink);
+
+    expect(aboutLink).toBeInTheDocument();
+
+    const logo = screen.getByRole('link', { name: / /i});
+    userEvent.click(logo);
+
+    expect(logo).toBeInTheDocument();
+
+    const HowItWorks = screen.getByRole('link', { name: /how it works/i });
+    userEvent.click(HowItWorks);
+
+    expect(HowItWorks).toBeInTheDocument();
+
+    // const search = screen.getByRole('link', { name: /searchpage/ });
+    // userEvent.click(search);
+
+    // expect(search).toBeInTheDocument();
+  });
 });
+
+describe('Hero', () => {
+  test(' clicking CTA button, subreddit, heatmap takes to search page', () => {
+    setup();
+    const Cta = screen.getByRole('button', { name: /Button/ });
+    userEvent.click(Cta);
+  });
+});
+
